bca4d57a2dba4241b8265e32e84fa869a055c2d2
haien <s18b701@shohoku.ac.jp>
2019-10-24 14:18:51 +0900

    Initial commit

---
+++ app/src/main/java/ishiicai/shohoku/ac/jp/get_sick_tweet_mixer/MainActivity.java
@@ -0,0 +1,13 @@
+package ishiicai.shohoku.ac.jp.get_sick_tweet_mixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
